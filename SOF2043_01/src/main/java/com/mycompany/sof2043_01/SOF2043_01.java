f3c12aeb20c1f31c7e31a9fe1de95847d7d0db27
huyennk6 <huyennk6@fpt.edu.vn>
2025-07-17 08:44:47 +0700

    chuan bi project

---
+++ SOF2043_01/src/main/java/com/mycompany/sof2043_01/SOF2043_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sof2043_01;
+
+/**
+ *
+ * @author Huyen
+ */
+public class SOF2043_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
